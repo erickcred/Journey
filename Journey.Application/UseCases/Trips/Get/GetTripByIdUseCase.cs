ebdd7e31b0caf85995d4b2c9e514b33dca9b2fcb
Erick Rick <erickcredjes@gmail.com>
2024-07-09 19:19:38 -0300

    Aplicando UseCase para TripGetById

---
+++ Journey.Application/UseCases/Trips/Get/GetTripByIdUseCase.cs
@@ -0,0 +1,39 @@
+﻿using Journey.Communication.Responses;
+using Journey.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journey.Application.UseCases.Trips.Get;
+
+public class GetTripByIdUseCase
+{
+  private readonly JourneyContext _journeyContext;
+
+  public GetTripByIdUseCase(JourneyContext journeyContext)
+  {
+    _journeyContext = journeyContext;
+  }
+
+  public ResponseTripJson Execute(Guid tripId)
+  {
+    var trip = _journeyContext
+      .Trips
+      .Include(a => a.Activities)
+      .AsNoTracking()
+      .FirstOrDefault(t => t.Id.Equals(tripId));
+
+    return new ResponseTripJson
+    {
+      Id = trip.Id,
+      Name = trip.Name,
+      StartDate = trip.StartDate,
+      EndDate = trip.EndDate,
+      Activities = trip.Activities.Select(a => new ResponseActivityJson
+      {
+        Id = a.Id,
+        Name = a.Name,
+        Date = a.Date,
+        Status = (Communication.Enums.ActivityStatus)a.Status
+      }).ToList()
+    };
+  }
+}
