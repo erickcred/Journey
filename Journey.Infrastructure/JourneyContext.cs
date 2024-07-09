1f33bb136bd7851df3f676debdccd8a4274e268a
Erick Rick <erickcredjes@gmail.com>
2024-07-09 17:40:57 -0300

    Refatorando Contexto

--- Journey.Infrastructure/JourneyContext.cs
+++ Journey.Infrastructure/JourneyContext.cs
@@ -1,10 +1,13 @@
 ﻿using Journey.Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace Journey.Infrastructure;
 
 public class JourneyContext : DbContext
 {
+  private readonly IConfiguration _config;
+
   public JourneyContext(DbContextOptions<JourneyContext> options) : base(options) { }
 
   public DbSet<Trip> Trips { get; set; }
