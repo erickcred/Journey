1fc8aa995ceec069da35868d43d1f834c77c5222
Erick Rick <erickcredjes@gmail.com>
2024-07-09 13:50:19 -0300

    Iniciando Tratamento de Exceções personalizadas

---
+++ Journey.Exception/ExceptionBase/JourneyException.cs
@@ -0,0 +1,8 @@
+﻿namespace Journey.Exception.ExceptionBase;
+
+public class JourneyException : SystemException
+{
+  public string Message { get; set; }
+
+  public JourneyException(string message) => Message = message;
+}
